d3117fd299714597949f7055f96fcd42dfdb2da2
edsoliveira72 <edsoliveira1972@gmail.com>
2026-09-23 23:15:15 -0300

    Delete Respostas_de_Desempenho_Comercial_e_Cobertura_de_Vendedores.sql

--- Respostas_de_Desempenho_Comercial_e_Cobertura_de_Vendedores.sql
+++
@@ -1,8 +0,0 @@
--- Pergunta 1 e 2: Vendas por estado do cliente e % de participação de vendedores locais
-SELECT 
-    customer_state AS `Estado Consumidor`,
-    receita_total_estado AS `Receita Total Consumida (R$)`,
-    total_itens_comercializados AS `Total Itens Adquiridos`,
-    pct_participacao_vendedor_local AS `Venda Local (% Atendida na UF)`
-FROM gold_olist.gold_metrics_desempenho_comercial
-ORDER BY receita_total_estado DESC;
